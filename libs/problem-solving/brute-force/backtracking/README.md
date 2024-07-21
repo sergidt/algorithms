a68ca574738fd6f7062040226c360c9b47a5317e
Sergi Dote Teixidor <sergidt@gmail.com>
2024-07-21 19:21:25 +0200

    init

---
+++ libs/problem-solving/brute-force/backtracking/README.md
@@ -0,0 +1,7 @@
+# backtracking
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test backtracking` to execute the unit tests.
---
+++ libs/problem-solving/brute-force/backtracking/src/lib/backtracking.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-backtracking',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './backtracking.component.html',
+  styleUrl: './backtracking.component.scss',
+})
+export class BacktrackingComponent {}
